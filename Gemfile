f403cb0a002e7da2529e531f6fb0fc2af0cdeebf
Tim Friedrich <friedrich.tim@googlemail.com>
2013-09-26 14:18:23 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+
+# Use sqlite3 as the database for Active Record
+group :development do
+	gem 'sqlite3'
+	gem 'rspec-rails', '2.9.0'
+end
+
+# Use SCSS for stylesheets
+group :assets do
+	gem 'sass-rails', '~> 4.0.0'
+	gem 'uglifier', '>= 1.3.0'
+	gem 'coffee-rails', '~> 4.0.0'
+end
+
+group :production do
+	gem 'pg', '0.12.2'
+end
+
+group :test do
+	gem 'capybara', '1.1.2'
+end
+
+gem 'jquery-rails'
+
+gem 'turbolinks'
+
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = 'b98245928b9fdf0ca78d03ffa30b5fb71e1844e1b50fd7d4379c4cd1fb2e2c8286abe872981afd24687ed979a3367ac419562003813b904105daba755aba9497'
