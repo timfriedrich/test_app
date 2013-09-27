9985dcf74b083181ccaf556426bd9afa2d6a392e
Tim Friedrich <friedrich.tim@googlemail.com>
2013-09-27 22:42:49 +0200

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 TestApp::Application.routes.draw do
+  get "users/new"
+	
 	root to: 'static_pages#home', via: [:get]
+	
+	match '/signup', 	to: 'users#new', via: [:get, :post]
 	
 	match '/help', 		to: 'static_pages#help', via: [:get]
 	match '/about',		to: 'static_pages#about', via: [:get]
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,21 +13,23 @@ describe "StaticPages" do
 	describe "Help page" do
 		before { visit help_path }
 		
-		it { page.should have_content('Help') }	
-		it { page.should have_selector('title', text: full_title('Help')) }
+		it { should have_content('Help') }	
+		it { should have_selector('title', text: full_title('Help')) }
 	end
 		
 	describe "About Page" do
 		before { visit about_path }
 		
-		it { page.should have_content('About Us') }
-		it { page.should have_selector('title', text: full_title('About')) }
+		it { should have_content('About Us') }
+		it { should have_selector('title', text: full_title('About')) }
 	end
 		
 	describe "Contact Page" do
 		before { visit contact_path }
 		
-		it { page.should have_selector('h1', text: 'Contact') }
-		it { page.should have_selector('title', text: full_title('Contact')) }
+		it { should have_selector('h1', text: 'Contact') }
+		it { should have_selector('title', text: full_title('Contact')) }
 	end
+	
+
 end
