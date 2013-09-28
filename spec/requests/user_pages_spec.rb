39ad8bf791c2649e8ffa010df6406683a35db92f
Tim Friedrich <friedrich.tim@googlemail.com>
2013-09-28 11:51:23 +0200

    asdf

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe "UserPages" do
 	subject { page }
+	
+	shared_examples_for "all User pages" do
+		it { should have_selector('h1', 	text: heading) }
+		it { should have_selector('title', 	text: full_title(page_title)) }
+	end
+	
     describe "Sign up" do 
 		before { visit signup_path }
+		let(:heading) { 'Sign up' }
+		let(:page_title) { 'Sign up' } 
 		
-		it { should have_selector('h1', text: 'Sign up') }
-		it { should have_selector('title', text: full_title('Sign up')) }
+		it_should_behave_like "all User pages"
 	end
 end
